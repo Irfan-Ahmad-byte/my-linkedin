000c3a8d609901cefb3fd708bb3265739f71558c
Irfan Ahmad <76489214+Irfan-Ahmad-byte@users.noreply.github.com>
2023-09-18 19:48:54 +0000

    changes in __init__

--- TimerTrigger2/__init__.py
+++ TimerTrigger2/__init__.py
@@ -8,14 +8,5 @@ from TimerTrigger2.sub_modules.azure_openai import setup_openai
 
 
 def main(mytimer: func.TimerRequest) -> None:
-    utc_timestamp = datetime.datetime.utcnow().replace(
-        tzinfo=datetime.timezone.utc).isoformat()
-
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
-
     setup_openai()
-
     main_post()
-
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
